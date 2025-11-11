632bfb9df0209838692526a9bbb1dda5fd5aacac
Romina Reyes <rominareyes022@gmail.com>
2025-11-11 19:55:44 -0300

    Agrego caso de uso y rename del test del caso de uso

--- core/src/test/java/usecase/CreateTaskUseCaseTest.java
+++ core/src/test/java/usecase/CreateTaskUseCaseTest.java
@@ -22,7 +22,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class test_useCaseCreateTask {
+public class CreateTaskUseCaseTest {
 
 
     @Mock
