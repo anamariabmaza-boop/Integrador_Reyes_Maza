e6e3c789384f1adad5c5715d190223cfa8445dba
Romina Reyes <rominareyes022@gmail.com>
2025-11-12 22:30:07 -0300

    Agregacion del C.U GetProjectSummary + Agregacion de test unitarios para probar la funcionalidad

---
+++ core/src/main/Java/output/TaskRepository.java
@@ -0,0 +1,11 @@
+package output;
+
+import model.Project;
+import model.Task;
+
+import java.util.List;
+
+public interface TaskRepository {
+    List<Task> findByProject(Project project);
+    Task saveTask(Task task);
+}
