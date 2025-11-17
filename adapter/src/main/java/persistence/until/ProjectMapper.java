c246ab20b3e65f9887eeb2b891bacfcb2125e2e2
ana <anamariabmaza@gmail.com>
2025-11-17 14:24:34 -0300

    Agregacion de persistencia Project (crud - entity - repository - until)

--- adapter/src/main/java/persistence/until/ProjectMapper.java
+++ adapter/src/main/java/persistence/until/ProjectMapper.java
@@ -1,4 +1,48 @@
 package persistence.until;
 
+import model.Project;
+import persistence.entity.ProjectData;
+
+import java.time.Clock;
+
 public class ProjectMapper {
+
+    // Convierte Project (dominio) → ProjectData (JPA)
+    public static ProjectData toData(Project project) {
+        if (project == null) return null;
+
+        ProjectData data = new ProjectData(
+                project.getName(),
+                project.getStartDate(),
+                project.getEndDate(),
+                project.getProjectStatus(),
+                project.getDescription()
+        );
+       // IMPORTANTE: mantener el ID si existe
+        if (project.getProjectId() != null) {
+            data.setId(project.getProjectId());
+        }
+/* Esta validacion vendria a ser para que no cree un nuevo proyecto cada vez
+       y que conserve el ID si ya existe
+       if (project.getProjectId() != null) {
+            ProjectData.setId(project.getProjectId());
+        }
+        Debo poner o no un setId??
+        */
+        return data;
+    }
+
+    // Convierte ProjectData (JPA) → Project (dominio)
+    public static Project toDomain(ProjectData data) {
+        if (data == null) return null;
+        return Project.newProject(
+                data.getId(),
+                data.getName(),
+                data.getStartDate(),
+                data.getEndDate(),
+                data.getProjectStatus(),
+                data.getDescription(),
+                Clock.systemDefaultZone()
+        );
+    }
 }
